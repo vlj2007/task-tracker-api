cf39427f4f6afda3300f6624192054ed05f50646
vladimir <vlj2007@yandex.ru>
2025-01-04 18:17:05 +0700

    Initial commit

---
+++ src/main/java/my/code/tasktrackerapi/TaskTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package my.code.tasktrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskTrackerApiApplication.class, args);
+    }
+
+}
