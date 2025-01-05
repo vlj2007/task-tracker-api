9ceeb660a15abd2d4b7b63ec5baa428d334b6cee
vladimir <vlj2007@yandex.ru>
2025-01-05 14:34:26 +0700

    I added file repository ProjectRepository.java

--- src/main/java/my/code/tasktrackerapi/store/repositories/ProjectRepository.java
+++ src/main/java/my/code/tasktrackerapi/store/repositories/ProjectRepository.java
@@ -2,7 +2,9 @@ package my.code.tasktrackerapi.store.repositories;
 
 import my.code.tasktrackerapi.store.entities.ProjectEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface ProjectRepository extends JpaRepository<ProjectEntity, Long> {
+    Optional<ProjectEntity> findByName(String name);
 
 }
